51c621993970af78c5b178648474633bff8de530
Alabhya Pandey <alabhyap@gmail.com>
2018-04-01 20:15:48 +0530

    Initial commit

---
+++ app/src/main/java/com/prajjawalan/prajjwalan/SplashScreen.java
@@ -0,0 +1,18 @@
+package com.prajjawalan.prajjwalan;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, GoogleSignIn.class);
+        startActivity(intent);
+        finish();
+    }
+}
+
---
+++ app/src/main/java/com/prajjawalan/prajjwalan/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.prajjawalan.prajjwalan.utils;
+
+/**
+ * Created by Alabhya Pandey on 21-03-2018.
+ */
+
+public class Constants {
+    public static final String USER_KEY = "users";
+}
